3b4a19ca8f929d1c7dfbb679700fe6970688e954
Bryan Brewer <brybrewe@cisco.com>
2019-04-25 17:28:03 +0000

    making these scripts more robust

--- app/tasks/apache.sh
+++ app/tasks/apache.sh
@@ -3,6 +3,8 @@ set -e
 
 echo '---- install Apache'
 
+function apt-get() { while fuser -s /var/lib/dpkg/lock; do echo 'apt-get is waiting for the lock release ...'; sleep 1; done; /usr/bin/apt-get "$@"; }
+
 DEBIAN_FRONTEND=noninteractive apt-get -y update
 DEBIAN_FRONTEND=noninteractive apt-get -y install apache2
 
--- app/tasks/docker.sh
+++ app/tasks/docker.sh
@@ -9,6 +9,8 @@ apt-key adv \
   --keyserver 'hkp://p80.pool.sks-keyservers.net:80' \
   --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
+function apt-get() { while fuser -s /var/lib/dpkg/lock; do echo 'apt-get is waiting for the lock release ...'; sleep 1; done; /usr/bin/apt-get "$@"; }
+
 DEBIAN_FRONTEND=noninteractive apt-get -y update
 DEBIAN_FRONTEND=noninteractive apt-get -y install docker-engine
 
